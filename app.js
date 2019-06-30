c869b079a40ff4715e978ec42bd99c4ea1cf984b
Tomer Ovadia <tomertomertomer@gmail.com>
2019-06-30 12:27:17 -0700

    Fix server

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.get('/', function (req, res) {
   res.sendFile( __dirname + "/" + "index.html" );
 });
 
-
 // Set up server
 var server = app.listen(process.env.PORT || 8081, () => {
    console.log("EffingVote listening at 8081");
