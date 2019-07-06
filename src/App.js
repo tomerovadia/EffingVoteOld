76bbb186aaa43cfc520d4791ac4fecc99d072000
Tomer Ovadia <tomertomertomer@gmail.com>
2019-07-06 14:31:51 -0700

    Add basic form to front-end and have it handle sending of messages

--- src/App.js
+++ src/App.js
@@ -10,18 +10,32 @@ class App extends React.Component {
   
   constructor(props) {
     super(props);
+    this.state = {
+      message: "",
+    };
     this.props.fetchBlah();
     this.props.fetchWSClient();
-    setTimeout(() => {
-      if (this.props.wsClient != null && this.props.wsClient.readyState === this.props.wsClient.OPEN) {
-        var number = Math.round(Math.random() * 0xFFFFFF);
-        this.props.wsClient.send(number.toString());
-      }
-    }, 5000);
+    // setTimeout(() => {
+    //   if (this.props.wsClient != null && this.props.wsClient.readyState === this.props.wsClient.OPEN) {
+    //     var number = Math.round(Math.random() * 0xFFFFFF);
+    //     this.props.wsClient.send(number.toString());
+    //   }
+    // }, 5000);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleMessageChange(e) {
+    this.setState({message: e.target.value});
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.wsClient.send(this.state.message);
   }
   
   render() {
-    console.log(this.props.wsClient);
+    // console.log(this.props.wsClient);
     return <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -30,6 +44,19 @@ class App extends React.Component {
             {this.props.blah}
           </p>
         </header>React.Component 
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            placeholder="placeholder" 
+            value={this.state.message}
+            onChange={this.handleMessageChange}
+          />
+          
+          <button>
+            Submit
+          </button>
+          
+        </form>
       </div>
   }
 }
