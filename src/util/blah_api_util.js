bbd089b5773480550aa334bd0e2a6de898931a9a
Tomer Ovadia <tomertomertomer@gmail.com>
2019-10-13 10:24:25 -0400

    Refactor chatroom functionality in server.js

--- src/util/blah_api_util.js
+++ src/util/blah_api_util.js
@@ -3,13 +3,16 @@ import { receiveMessage } from '../index';
 const io = require('socket.io-client');
 
 export const fetchSocketClient = () => {
+  console.log("fetchSocketClient");
   return new Promise((resolve, reject) => {
+    console.log("fetchSocketClient Promise");
     const socket = io();
     socket.on('message', receiveMessage);
     resolve(socket);
   });
 }
- 
+
+// Example API call
 export const getBlah = (availability) => {
   return $.ajax({
     method: 'get',
